39a1c4835c28dc32de164381faf9ac1958da1ece
MA Jian <62880254+majianthu@users.noreply.github.com>
2021-01-20 03:49:59 +0800

    Update and rename ts1.r to te1.r

--- te1.r
+++ te1.r
@@ -3,16 +3,15 @@ prsa2010data = read.csv("https://archive.ics.uci.edu/ml/machine-learning-databas
 # id: 6(PM2.5), 7(Dew Point), 8(Temperature), 9(Pressure), 11(Cumulative Wind Speed)
 idx = c(6,9)
 data = prsa2010data[2200:2700, idx]
-tslag = 0
+telag = 0
 for (lag in 1:24){
   pm25a = data[1:(501-lag),1]
   pm25b = data[(lag+1):501,1]
   v1 = data[1:(501-lag),2]
   data1 = cbind(pm25a, pm25b, v1)
-  
   # estimating transfer entropy via copula entropy
-  tslag[lag] = copent(data1) - copent(data1[,c(1,2)]) - copent(data1[,c(1,3)]) 
+  telag[lag] = copent(data1) - copent(data1[,c(1,2)]) - copent(data1[,c(1,3)]) 
 }
 x11()
-plot(tslag, xlab = "lag (hours)", ylab = "Transfer Entropy", main = "Pressure")
-lines(tslag)
+plot(telag, xlab = "lag (hours)", ylab = "Transfer Entropy", main = "Pressure")
+lines(telag)
