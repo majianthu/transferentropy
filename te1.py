5b2c78938674433913fd1ca60e4c94d802d40c7b
MA Jian <majian1979@qq.com>
2022-02-05 08:43:52 +0800

    add knncmi, pycit

--- te1.py
+++ te1.py
@@ -1,5 +1,7 @@
-from copent import transent
-from pandas import read_csv
+from copent import transent, ci
+import knncmi as k
+from pycit import citest
+from pandas import read_csv, DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,15 +11,29 @@ prsa2010 = read_csv(url)
 data = prsa2010.iloc[2200:2700,[5,8]].values
 
 te = np.zeros(24)
+cmi1 = np.zeros(24)
+ci1 = np.zeros(24)
 for lag in range(1,25):
-	te[lag-1] = transent(data[:,0],data[:,1],lag)
-	str = "TE from pressure to PM2.5 at %d hours lag : %f" %(lag,te[lag-1])
+	x1 = data[0:(500-lag),0]
+	x2 = data[lag:500,0]
+	y = data[0:(500-lag),1]
+	df = DataFrame(np.vstack((x2,y,x1)).T, columns = ['x2','y','x1'])
+	
+	te[lag-1] = ci(x2,y,x1)
+	# te[lag-1] = transent(data[:,0],data[:,1],lag)
+	cmi1[lag-1] = k.cmi(['x2'], ['y'], ['x1'], 3, df)
+	ci1[lag-1] = citest(x2, y, x1, test_args={'statistic': 'ksg_mi'})
+	
+	str = "lag : %d; TE : %f; CMI : %f; CI : %f" %(lag,te[lag-1],cmi1[lag-1],ci1[lag-1])
 	print(str)
 	
-plt.plot(te)
+plt.plot(te,'g',label = 'copent')
+plt.plot(cmi1,'r',label = 'knncmi')
+plt.plot(ci1,'b', label = 'pycit')
 plt.title("Pressure on PM2.5")
 plt.xlabel('lag(hours)')
 pos = np.array([4,9,14,19])
 plt.xticks(pos,pos+1)
 plt.ylabel('Transfer Entropy')
-plt.show()	
+plt.legend()
+plt.show()		
