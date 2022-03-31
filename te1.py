4baf9b7535da68f9fb91c436c3be2d010068a97a
MA Jian <majian1979@qq.com>
2022-03-31 18:20:10 +0800

    add PCIT

--- te1.py
+++ te1.py
@@ -4,6 +4,7 @@ from causallearn.utils.KCI.KCI import KCI_CInd
 from pycit import citest
 from fcit import fcit
 from CCIT import CCIT
+from pcit.IndependenceTest import PCIT  ##  the 'mlxtend' package required
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,6 +23,7 @@ kci1 = np.zeros(24)
 ci1 = np.zeros(24)
 fcit1 = np.zeros(24)
 ccit1 = np.zeros(24)
+pcit1 = np.zeros(24)
 for lag in range(1,25):
 	x1 = data[0:(500-lag),0]
 	x2 = data[lag:500,0]
@@ -46,8 +48,11 @@ for lag in range(1,25):
 	kci1[lag-1],_ = kci_cind.compute_pvalue(x2a,ya,x1a)
 	## CCIT
 	ccit1[lag-1] = CCIT.CCIT(x2a,ya,x1a)
+	## pcit
+	p,tmp,tmp = PCIT(x2a,ya,z = x1a)
+	pcit1[lag-1] = p[0]
 	
-	str = "lag: %d; TE: %.3f; CMI: %.3f; KCI: %.3f; CI: %.3f; fcit: %.3f; ccit: %.3f" %(lag,te1[lag-1],cmi1[lag-1],kci1[lag-1],ci1[lag-1],fcit1[lag-1],ccit1[lag-1])
+	str = "lag: %d; TE:%.3f; CMI:%.3f; KCI:%.3f; CI:%.3f; fcit:%.3f; ccit:%.3f; pcit:%.3f" %(lag,te1[lag-1],cmi1[lag-1],kci1[lag-1],ci1[lag-1],fcit1[lag-1],ccit1[lag-1],pcit1[lag-1])
 	print(str)
 	
 plt.plot(te1,'g',label = 'copent')
@@ -56,6 +61,7 @@ plt.plot(kci1,'c', label = 'causal-learn')
 plt.plot(ci1,'b', label = 'pycit')
 plt.plot(fcit1,'y', label = 'fcit')
 plt.plot(ccit1,'m', label = 'ccit')
+plt.plot(pcit1,'k', label = 'pcit')
 title = "%s on %s" % (varname[cid],varname[eid])
 plt.title(title)
 plt.xlabel('lag(hours)')
